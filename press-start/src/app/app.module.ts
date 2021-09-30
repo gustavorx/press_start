c940cc9cf48c0925240e0d08cfb48d91fb287984
Rooney <roneysantos7711@gmail.com>
2021-09-29 21:46:10 -0300

    Subindo front de login e cadastro

--- press-start/src/app/app.module.ts
+++ press-start/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { AlterarCadastroComponent } from './components/alterar-cadastro/alterar-cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CadastroComponent,
+    AlterarCadastroComponent
   ],
   imports: [
     BrowserModule
---
+++ press-start/src/app/components/alterar-cadastro/alterar-cadastro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alterar-cadastro',
+  templateUrl: './alterar-cadastro.component.html',
+  styleUrls: ['./alterar-cadastro.component.css']
+})
+export class AlterarCadastroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
