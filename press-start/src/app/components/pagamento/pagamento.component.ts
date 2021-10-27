064f673590755d2895620d6be1d5b784c6aaede7
ViniciusMarques <msvinicius27@gmail.com>
2021-10-27 20:24:47 -0300

    opcoes de pagamento e tela de boleto

--- press-start/src/app/components/pagamento/pagamento.component.ts
+++ press-start/src/app/components/pagamento/pagamento.component.ts
@@ -8,4 +8,37 @@ import { Component } from '@angular/core';
 
 export class PagamentoComponent {
     
+  esconderDescricao = false;
+  esconderCartao = true;
+  esconderBoleto = true;
+  esconderPix = true;
+  esconderMetodos = false;
+
+  mostrarCartao(){
+    console.log("jooj");
+    this.esconderCartao = false;
+    this.esconderMetodos = true;
+    this.esconderDescricao = true;
+  }
+
+  mostrarBoleto(){
+    console.log("jooj");
+    this.esconderBoleto = false;
+    this.esconderMetodos = true;
+    this.esconderDescricao = true;
+  }
+
+  mostrarPix(){
+    this.esconderPix = false;
+    this.esconderMetodos = true;
+    this.esconderDescricao = true;
+  }
+
+  alterarPagameto(){
+    this.esconderCartao = true;
+    this.esconderBoleto = true;
+    this.esconderPix = true;
+    this.esconderMetodos = false;
+    this.esconderDescricao = false;
+  }
 }
