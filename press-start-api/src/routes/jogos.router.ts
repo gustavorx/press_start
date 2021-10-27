17ed90f3cd79b6f2767e947461ffd73419991e7d
Gustavo Navarro <gustavo_navarro2000@hotmail.com>
2021-10-27 19:05:18 -0300

    finaliza a API de jogos

--- press-start-api/src/routes/jogos.router.ts
+++ press-start-api/src/routes/jogos.router.ts
@@ -24,8 +24,8 @@ jogosRouter.get("/:id", async (req: Request, res: Response) => {
         const query = { _id: new ObjectId(id) };
         const jogo = (await collections.jogos?.findOne(query)) as Jogo;
 
-        if (jogo)
-            res.status(200).send(jogo);
+        if (jogo) res.status(200).send(jogo);
+        else res.status(402).send(`Jogo ${id} não encontrado `)
     } catch (error) {
         res.status(500).send(`Erro ao buscar o jogo com ID ${id}: ${error}`)
     }
@@ -49,5 +49,41 @@ jogosRouter.post("/", async (req: Request, res: Response) => {
 });
 
 // PUT
+jogosRouter.put("/:id", async (req: Request, res: Response) => {
+    const id = req?.params?.id;
+
+    try {
+        const jogoAtualizado: Jogo = req.body as Jogo;
+        const query = { _id: new ObjectId(id) };
+
+        const result = await collections.jogos?.updateOne(query, { $set: jogoAtualizado });
+
+        result
+            ? res.status(200).send(`Jogo ${id} atualizado com sucesso`)
+            : res.status(304).send(`Jogo ${id} não foi atualizado`);
+    } catch (error: any) {
+        console.error(error.message);
+        res.status(400).send(error.message);
+    }
+});
 
 // DELETE
+jogosRouter.delete("/:id", async (req: Request, res: Response) => {
+    const id = req?.params?.id;
+
+    try {
+        const query = { _id: new ObjectId(id) };
+        const result = await collections.jogos?.deleteOne(query);
+
+        if (result && result.deletedCount) {
+            res.status(202).send(`Jogo ${id} removido com sucesso`);
+        } else if (!result) {
+            res.status(400).send(`Falha ao remover o jogo ${id}`);
+        } else if (!result.deletedCount) {
+            res.status(404).send(`Jogo com id ${id} não existe`);
+        }
+    } catch (error: any) {
+        console.error(error.message);
+        res.status(400).send(error.message);
+    }
+});
