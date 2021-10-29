963cf06bd04a693139dd5bb28e73ca2f42a110e3
ViniciusMarques <msvinicius27@gmail.com>
2021-10-29 16:01:54 -0300

    dados do pedido adicionados à tela de pagamento. funcoes pix e codigo de barras boleto

--- press-start/src/app/components/pagamento/boleto/boleto.component.ts
+++ press-start/src/app/components/pagamento/boleto/boleto.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 
 @Component({
@@ -7,6 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./boleto.component.css']
 })
 
-export class BoletoComponent {
-    
+export class BoletoComponent implements OnInit {
+  
+  esconderImprimir: boolean = false;
+  codigo: string = "";
+  
+  ngOnInit(): void {
+    this.gerarCodigoPix();
+  }
+  
+  imprimir(){
+      this.esconderImprimir = true;
+      setTimeout(function(){ 
+        window.print();
+       }, 500);   
+  }
+
+  gerarCodigoPix(){
+    var part1: string = this.stringAleatoria(5, '0123456789');
+    var part2: string = this.stringAleatoria(5, '0123456789');
+    var part3: string = this.stringAleatoria(5, '0123456789');
+    var part4: string = this.stringAleatoria(6, '0123456789');
+    var part5: string = this.stringAleatoria(5, '0123456789');
+    var part6: string = this.stringAleatoria(6, '0123456789');
+    var part7: string = this.stringAleatoria(1, '0123456789');
+    var part8: string = this.stringAleatoria(15, '0123456789');
+
+    this.codigo = part1 + "." + part2 + "." + part3 + "." + part4 + " " + part5 + "." + part6 + " " + part7 + " " + part8;
+  }
+
+  stringAleatoria(length: number, chars: string | any[]) {
+    var result = '';
+    for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+    return result;
+  }
+
 }
--- press-start/src/app/components/pagamento/pagamento.component.ts
+++ press-start/src/app/components/pagamento/pagamento.component.ts
@@ -7,12 +7,22 @@ import { Component } from '@angular/core';
 })
 
 export class PagamentoComponent {
-    
+   
   esconderDescricao = false;
   esconderCartao = true;
   esconderBoleto = true;
   esconderPix = true;
   esconderMetodos = false;
+
+  numero: string = "";
+  validade: string = "";
+  cvv: string = "";
+  nomeCompleto: string = "";
+  cpf: string = "";
+  telefone: string = "";
+  nascimento: string = "";
+  qrCode: string = "";
+
 
   mostrarCartao(){
     console.log("jooj");
@@ -32,6 +42,7 @@ export class PagamentoComponent {
     this.esconderPix = false;
     this.esconderMetodos = true;
     this.esconderDescricao = true;
+    this.gerarCodigoPix()
   }
 
   alterarPagameto(){
@@ -40,5 +51,40 @@ export class PagamentoComponent {
     this.esconderPix = true;
     this.esconderMetodos = false;
     this.esconderDescricao = false;
+  }
+
+  comprarCredito(){
+    
+  }
+
+  // validarCPF(){
+  //   if()
+  // }
+
+  gerarCodigoPix(){
+    var part1: string = this.stringAleatoria(14, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    var part2: string = this.stringAleatoria(12, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    var part3: string = this.stringAleatoria(4, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    var part4: string = this.stringAleatoria(4, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    var part5: string = this.stringAleatoria(4, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    var part6: string = this.stringAleatoria(45, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    var part7: string = this.stringAleatoria(8, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    var part8: string = this.stringAleatoria(19, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+
+    this.qrCode = part1 + "brgovbcbpix" + part2 + "-" + part3 + "-" + part4 + "-" + part5 + part6 + "PRES_START6009SaoPauloBR" + part7 + "mpqrinter" + part8;
+  }
+
+  copiarCodigo(){
+    var copyText: string = this.qrCode;
+  
+    navigator.clipboard.writeText(copyText);
+  
+    alert("Copied the text: " + copyText);
+  }
+
+  stringAleatoria(length: number, chars: string | any[]) {
+    var result = '';
+    for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+    return result;
   }
 }
